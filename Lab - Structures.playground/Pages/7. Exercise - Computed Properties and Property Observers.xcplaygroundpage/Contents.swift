0befc4f9d2578acf4814cf308b87e05af1c5ff5e
Melanie-ai <ma.melanielaveriano@gmail.com>
2024-10-21 12:01:41 -0400

    gghjghj

--- Lab - Structures.playground/Pages/7. Exercise - Computed Properties and Property Observers.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/7. Exercise - Computed Properties and Property Observers.xcplaygroundpage/Contents.swift
@@ -6,9 +6,12 @@
 struct Rectangle {
     var width: Int
     var height: Int
-    
+    var area: Int {
+        return width * height
+    }
 }
-
+var myRect = Rectangle(width: 10, height: 20)
+print(myRect.area)
 
 /*:
  In the `Height` struct below, height is represented in both inches and centimeters. However, if `heightInInches` is changed, `heightInCentimeters` should also adjust to match it. Add a `didSet` to each property that will check if the other property is what it should be, and if not, sets the proper value. If you set the value of the other property even though it already has the right value, you will end up with an infinite loop of each property setting the other.
@@ -30,7 +33,11 @@ struct Height {
         self.heightInInches = heightInCentimeters/2.54
     }
 }
+var myHeight = Height(heightInCentimeters: 181)
+print(myHeight.heightInInches)
 
+myHeight.heightInCentimeters = 170
+print(myHeight.heightInInches)
 
 
 /*:
--- Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
@@ -4,10 +4,15 @@
  Imagine you have an app that requires the user to log in. You may have a `User` struct similar to that shown below. However, in addition to keeping track of specific user information, you might want to have a way of knowing who the current logged in user is. Create a `currentUser` type property on the `User` struct below and assign it to a `user` object representing you. Now you can access the current user through the `User` struct. Print out the properties of `currentUser`.
  */
 struct User {
-        
     var userName: String
     var email: String
     var age: Int
+    static var currentUser = User(userName: "goran", email: "nesto", age: 17)
+    
+    static func logIn(user: User) {
+        currentUser = user
+        print("User \(currentUser) has logged in!")
+    }
 }
 
 
@@ -17,6 +22,12 @@ struct User {
  Below, call the `logIn(user:)` method and pass in a different `User` instance than what you assigned to currentUser above. Observe the printout in the console.
  */
 
+
+var me = User(userName: "goran", email: "goran.brlas@gmail.com", age:  17)
+User.currentUser = me
+
+var secondUser = User(userName: "ivan", email: "ivan@gmail.com", age: 10)
+User.logIn(user: secondUser)
 
 /*:
 [Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Type Properties and Methods](@next)
